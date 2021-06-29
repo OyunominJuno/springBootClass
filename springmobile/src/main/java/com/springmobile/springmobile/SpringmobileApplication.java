8c70dbb811d1fd8952f1fee2cdce7344ab18f10b
OyunominJuno <oyunominmun@gmail.com>
2021-06-29 15:33:03 -0700

    initial set up and first API

---
+++ springmobile/src/main/java/com/springmobile/springmobile/SpringmobileApplication.java
@@ -0,0 +1,13 @@
+package com.springmobile.springmobile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmobileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmobileApplication.class, args);
+	}
+
+}
